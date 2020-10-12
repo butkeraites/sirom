a5869b79b14839f5649feafefb7be0caf0942315
Renan Brito Butkeraites <rbritobut?@gmail.com>
2020-10-12 16:11:52 -0300

    Test Dimension validation feature

--- test_optimization_problem.py
+++ test_optimization_problem.py
@@ -16,3 +16,19 @@ def test_optimization_problem_failed():
 def test_optimization_problem_objective_coeficient_empty():
     opt_problem = OptimizationProblem("",A_value,b_value)
     assert "[ERROR] Undefined objective coeficient" in opt_problem.status
+
+def test_optimization_problem_constraint_coeficient_empty():
+    opt_problem = OptimizationProblem(c_value,"",b_value)
+    assert "[ERROR] Undefined constraint coeficient" in opt_problem.status
+
+def test_optimization_problem_rhs_coeficient_empty():
+    opt_problem = OptimizationProblem(c_value,A_value,"")
+    assert "[ERROR] Undefined rhs coeficient" in opt_problem.status
+
+def test_optimization_problem_dimension_validation_error():
+    opt_problem = OptimizationProblem("",A_value,b_value)
+    assert "[ERROR] Dimension can not be evaluated" in opt_problem.status
+
+def test_optimization_problem_dimension_inconsistency_error():
+    opt_problem = OptimizationProblem([1],A_value,b_value)
+    assert "[ERROR] Dimension inconsistency detected" in opt_problem.status
